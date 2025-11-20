4e27d33db0a9b964b11af9634894bec6e453deb0
BasemGhasan <BasemGhasan@gmail.com>
2025-11-20 09:03:51 +0800

    Setup the game board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+"use client"
+import { useState } from "react";
+import Cell from "./components/cell";
+
 export default function Home() {
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
   return (
     <div className="animated-gradient">
       <div className="root">
         <h1 className="header"> Tic-Tac-Toe Game</h1>
-        <div className="board"></div>
+        <div className="board">
+          {cells.map((cell, index) => (
+            <Cell key={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
