c368fc632bee1a5d119e0967b82c7aac8a1c5dc5
BasemGhasan <BasemGhasan@gmail.com>
2025-11-27 21:41:08 +0800

    Added interactive tic-tac-toe cell functionality

--- src/app/components/cell.tsx
+++ src/app/components/cell.tsx
@@ -1,5 +1,26 @@
-export default function Cell (){
-    return(
-        <div className="cell"></div>
-    );
+import { Dispatch, SetStateAction } from "react";
+
+type CellProps = {
+  id: number;
+  cells: string[];
+  setCells: Dispatch<SetStateAction<string[]>>;
+  go: string;
+  setGo: Dispatch<SetStateAction<string>>;
+};
+
+export default function Cell({ id, cells, setCells, go, setGo }: CellProps) {
+  const handleClick = () => {
+    const taken = !!cells[id];
+    console.log(taken);
+    const newCells = [...cells];
+    newCells[id] = go;
+    setCells(newCells);
+    setGo(go === "X" ? "O" : "X");
+  };
+
+  return (
+    <div className="cell" onClick={handleClick}>
+      {cells[id]}
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
-"use client"
+"use client";
 import { useState } from "react";
 import Cell from "./components/cell";
 
 export default function Home() {
   const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [go, setGo] = useState("X");
   return (
     <div className="animated-gradient">
       <div className="root">
         <h1 className="header"> Tic-Tac-Toe Game</h1>
         <div className="board">
           {cells.map((cell, index) => (
-            <Cell key={index} />
+            <Cell
+              id={index}
+              key={index}
+              cells={cells}
+              setCells={setCells}
+              go={go}
+              setGo={setGo}
+            />
           ))}
         </div>
       </div>
